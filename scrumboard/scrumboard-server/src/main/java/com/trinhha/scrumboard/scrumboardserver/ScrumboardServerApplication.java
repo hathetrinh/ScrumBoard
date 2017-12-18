e77c601a32f487cc1ec078958597437438bbf41b
trinhha <hathetrinh@gmail.com>
2017-12-18 10:49:28 +0700

    Add table Task

--- scrumboard/scrumboard-server/src/main/java/com/trinhha/scrumboard/scrumboardserver/ScrumboardServerApplication.java
+++ scrumboard/scrumboard-server/src/main/java/com/trinhha/scrumboard/scrumboardserver/ScrumboardServerApplication.java
@@ -1,6 +1,6 @@
 package com.trinhha.scrumboard.scrumboardserver;
 
- import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
